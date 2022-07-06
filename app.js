3a57425a889a6b3079ae4dcfb37eded2707e5fdf
barbiesan <barbarasolre@gmail.com>
2022-07-06 12:29:17 +0200

    fixed bugs

--- app.js
+++ app.js
@@ -54,4 +54,4 @@ mongoose
   })
   .catch(err => {
     console.log(err);
-  });
+  }); 
--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,5 +1,5 @@
+const { uuid } = require('uuidv4');
 const fs = require('fs');
-
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 
@@ -36,7 +36,7 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  // let places;
+
   let userWithPlaces;
   try {
     userWithPlaces = await User.findById(userId).populate('places');
@@ -48,7 +48,6 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  // if (!places || places.length === 0) {
   if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError('Could not find places for the provided user id.', 404)
